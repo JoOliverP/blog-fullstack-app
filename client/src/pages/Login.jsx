b650176ad0eff55d33c2b26ccac47023fc51c4d4
JoOliverP <joypinheiro63@gmail.com>
2022-09-27 18:57:23 -0300

    feat: integration api routes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,14 +1,39 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export function Login() {
+   const [inputs, setInputs] = useState({
+      username: "",
+      password:"",
+   })
+
+   const [err,setError] = useState(null);
+
+   const navigate = useNavigate()
+
+   function handleChange(e){
+      setInputs(prev => ({...prev, [e.target.name]: e.target.value}))
+   }
+
+   async function handleSubmit(event){
+     event.preventDefault()
+      try {
+         await axios.post("/auth/login",inputs)
+         navigate("/")
+      } catch (err) {
+         setError(err.response.data)
+      }
+
+   }
    return (
       <div className="auth">
          <h1>Login</h1>
          <form>
-            <input required type="text" placeholder="username" />
-            <input required type="password" placeholder="password" />
-            <button>Login</button>
-            <p>Ocorreu um erro!</p>
+            <input required type="text" placeholder="username" name="username"  onChange={handleChange}/>
+            <input required type="password" placeholder="password"  name="password" onChange={handleChange}/>
+            <button onClick={handleSubmit}>Login</button>
+            {err && <p>Ocorreu um erro!</p>}
             <span>Não possui conta ? <Link to="/register">Register</Link></span>
          </form>
       </div>
